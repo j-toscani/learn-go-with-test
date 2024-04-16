edc3ad5320da047990b7ccda6df0e6d9435c40e1
Julian Toscani <julian.toscani@grapes.de>
2024-04-16 23:17:50 +0200

    feat: add default behaviour

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("saying Hello to peaople", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
 
+		assertCorrectMesssage(got, want, t)
+	})
+
+	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		assertCorrectMesssage(got, want, t)
+	})
+}
+
+func assertCorrectMesssage(got string, want string, t testing.TB) {
+	t.Helper() // tells go this is a test helper
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
