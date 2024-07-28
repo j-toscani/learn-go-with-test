10985381b66319d22a152e9c686203adbd46d42a
Julian Toscani <julian.toscani@grapes.de>
2024-07-28 17:48:22 +0200

    feat: methods

--- structs-methods-interfaces/structs_methods_interfaces_test.go
+++ structs-methods-interfaces/structs_methods_interfaces_test.go
@@ -13,11 +13,26 @@ func TestPerimiter(t *testing.T) {
 }
 
 func TestArea(t *testing.T) {
-	rectangle := Rectangle{5.0, 10.0}
-	got := Area(rectangle)
-	want := 50.0
 
-	if got != want {
-		t.Errorf("got %.2f, want %.2f", got, want)
-	}
+	t.Run("rectangle", func(t *testing.T) {
+
+		rectangle := Rectangle{5.0, 10.0}
+		got := rectangle.Area()
+		want := 50.0
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
+
+	t.Run("circle", func(t *testing.T) {
+
+		circle := Circle{10.0}
+		got := circle.Area()
+		want := 314.1592653589793
+
+		if got != want {
+			t.Errorf("got %.2f, want %.2f", got, want)
+		}
+	})
 }
