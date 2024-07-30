6d5bf39aeb3e2581441abcc6617bdd15fa747965
Julian Toscani <julian.toscani@grapes.de>
2024-07-30 22:21:33 +0200

    feat: create search functionality and errors

---
+++ maps/dictionary_test.go
@@ -0,0 +1,40 @@
+package dictionary
+
+import "testing"
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertString(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+
+		if got == nil {
+			t.Fatal("expected to get an error.")
+		}
+
+		assertError(t, got, ErrorNotFound)
+	})
+}
+
+func assertString(t testing.TB, got, want string) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
