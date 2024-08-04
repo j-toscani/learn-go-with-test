4227795f2506cfd3a923b0f330bebb625e2a8233
Julian Toscani <julian.toscani@grapes.de>
2024-08-04 21:47:36 +0200

    feat: implement errors for dictionary

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,17 +1,39 @@
 package dictionary
 
-import "errors"
-
 type Dictionary map[string]string
 
-var ErrorNotFound = errors.New("could not find the word you were looking for")
+const (
+	ErrNotFound   = DictionaryErr("could not find the word you were looking for")
+	ErrWordExists = DictionaryErr("the word you want to ad already exists")
+)
+
+type DictionaryErr string
+
+func (e DictionaryErr) Error() string {
+	return string(e)
+}
 
 func (d Dictionary) Search(word string) (string, error) {
 	definition, ok := d[word]
 
 	if !ok {
-		return "", ErrorNotFound
+		return "", ErrNotFound
 	}
 
 	return definition, nil
+}
+
+func (d Dictionary) Add(word string, definition string) error {
+	_, err := d.Search(word)
+
+	switch err {
+	case ErrNotFound:
+		d[word] = definition
+	case nil:
+		return ErrWordExists
+	default:
+		return err
+	}
+
+	return nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -19,8 +19,42 @@ func TestSearch(t *testing.T) {
 			t.Fatal("expected to get an error.")
 		}
 
-		assertError(t, got, ErrorNotFound)
+		assertError(t, got, ErrNotFound)
 	})
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("adding a word", func(t *testing.T) {
+		dictionary := Dictionary{}
+		word := "test"
+		description := "this is just a test"
+
+		err := dictionary.Add(word, description)
+
+		assertError(t, err, nil)
+		assertDefinition(t, dictionary, word, description)
+	})
+
+	t.Run("word already exists", func(t *testing.T) {
+		word := "test"
+		definition := "this is just a test"
+
+		dictionary := Dictionary{word: definition}
+
+		err := dictionary.Add(word, definition)
+		assertError(t, err, ErrWordExists)
+		assertDefinition(t, dictionary, word, definition)
+	})
+}
+
+func assertDefinition(t testing.TB, dictionary Dictionary, word, description string) {
+	got, err := dictionary.Search(word)
+
+	if err != nil {
+		t.Fatal("should find added word", err)
+	}
+
+	assertString(t, got, description)
 }
 
 func assertString(t testing.TB, got, want string) {
