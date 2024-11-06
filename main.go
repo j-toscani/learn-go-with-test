7b1ae6946a54b3d4471f5a26bd234292eedc18cc
Julian Toscani <julian.toscani@grapes.de>
2024-11-06 16:26:14 +0100

    feat: learn about select and httptest

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	mocking "main/mocking"
+	"fmt"
 	"os"
-	"time"
 )
 
 func main() {
-	sleeper := &mocking.ConfigurableSleeper{Duration: 5 * time.Second, Sleeper: time.Sleep}
-	mocking.Countdown(os.Stdout, sleeper)
+	lesson := os.Args[1]
+
+	createFile := func(name string, content string) {
+		d := []byte(content)
+		os.WriteFile(name, d, 0644)
+	}
+
+	err := os.Mkdir(lesson, 0755)
+
+	if err != nil {
+		panic(err)
+	}
+
+	createFile(fmt.Sprintf("%s/%s.go", lesson, lesson), fmt.Sprintf("package %s", lesson))
+	createFile(fmt.Sprintf("%s/%s_test.go", lesson, lesson), fmt.Sprintf("package %s\n\nimport (\n  \"testing\"\n)\n\nfunc Test(t *testing.T) {}", lesson))
 }
