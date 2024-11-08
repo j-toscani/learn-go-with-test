f74d66a69f12d82bf4f896b086104592bb7a9c19
Julian Toscani <julian.toscani@grapes.de>
2024-11-08 12:23:55 +0100

    feat: add reflection

--- reflection/reflection.go
+++ reflection/reflection.go
@@ -3,7 +3,52 @@ package reflection
 import "reflect"
 
 func walk(x interface{}, fn func(string)) {
+	val := getValue(x)
+
+	walkValue := func(value reflect.Value) {
+		walk(value.Interface(), fn)
+	}
+
+	switch val.Kind() {
+	case reflect.Slice, reflect.Array:
+		for i := 0; i < val.Len(); i++ {
+			walkValue(val.Index(i))
+		}
+	case reflect.Struct:
+		for i := 0; i < val.NumField(); i++ {
+			walkValue(val.Field(i))
+		}
+	case reflect.Map:
+		for _, key := range val.MapKeys() {
+			walkValue(val.MapIndex(key))
+		}
+	case reflect.Chan:
+		// If channel returns value handle it, otherwise break loop
+		for {
+			if v, ok := val.Recv(); ok {
+				walkValue(v)
+			} else {
+				break
+			}
+		}
+	case reflect.Func:
+		// Calls the provided function with the argument `nil`
+		returnV := val.Call(nil)
+
+		for _, result := range returnV {
+			walkValue(result)
+		}
+	case reflect.String:
+		fn(val.String())
+	}
+}
+
+func getValue(x interface{}) reflect.Value {
 	val := reflect.ValueOf(x)
-	field := val.Field(0)
-	fn(field.String())
+
+	if val.Kind() == reflect.Pointer {
+		val = val.Elem()
+	}
+
+	return val
 }
